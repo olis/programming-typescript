556a6c4e3481242549c87967c6d01d296ae6180f
Oliver Schemmel <oliver.schemmel@web.de>
2021-10-22 17:20:48 +0200

    answers for chapter 3

---
+++ src/chapter-3/index.ts
@@ -0,0 +1,14 @@
+// Exercises
+
+const chapter3 = () => {
+
+    // 1. For each of these values, what type will TypeScript infer?
+    let a = 1042                    // number
+    let b = 'apples and oranges'    // string
+    const c = 'pineapples'          // 'pineapples'
+    let d = [true, true, false]     // boolean[]
+    let e = {type: 'ficus'}         // {type: string}
+    let f = [1, false]              // (number | boolean)[]
+    const g = [3]                   // number[]
+    let h = null                    // null --> any
+}
